1b24c160c7c50f33f6a26520077fb8561d1ef733
tkr <kgtkr.jp@gmail.com>
2018-05-02 21:58:31 +0900

    テスト

--- memory.spec.js
+++ memory.spec.js
@@ -1,5 +1,35 @@
 const child_process = require('child_process');
 const fs = require("fs");
+const abEq = require("arraybuffer-equal");
+
+expect.extend({
+  abEq(x, y) {
+    const pass = abEq(x, y);
+    if (pass) {
+      return {
+        message: () => `expected pass`,
+        pass: true,
+      };
+    } else {
+      let msg = "";
+      const a = new Int8Array(x);
+      const b = new Int8Array(y);
+      if (a.length !== b.length) {
+        msg = `length expect:${b.length} value:${a.length}`;
+      } else {
+        for (let i = 0; i < a.length; i++) {
+          if (a[i] !== b[i]) {
+            msg += `\nindex:${i} expect:${b[i]} value:${a[i]}`;
+          }
+        }
+      }
+      return {
+        message: () => `expected ${msg}`,
+        pass: false,
+      };
+    }
+  },
+});
 
 describe("memory", () => {
   var mod;
@@ -28,9 +58,9 @@ describe("memory", () => {
     const memSim = new DataView(memSimBuf);
 
     expect(wasm.exports.malloc(20)).toBe(2);
-    memSim.setInt8(2, 2);
-    memSim.setInt32(3, 20);
-    memSim.setInt32(7, -1);
-    expect(memory.buffer).toEqual(memSimBuf);
+    memSim.setInt8(2, 2, true);
+    memSim.setInt32(3, 20, true);
+    memSim.setInt32(7, -1, true);
+    expect(memory.buffer).abEq(memSimBuf);
   });
 });

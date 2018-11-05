d021baa3b232593e9d0e30585fc2edd6e331725a
kgtkr <kgtkr.jp@gmail.com>
2018-11-05 19:08:48 +0900

    余計なコード削除

--- memory.spec.js
+++ memory.spec.js
@@ -1,35 +1,5 @@
 const child_process = require('child_process');
 const fs = require("fs");
-const abEq = require("arraybuffer-equal");
-
-expect.extend({
-  abEq(x, y) {
-    const pass = abEq(x, y);
-    if (pass) {
-      return {
-        message: () => `expected pass`,
-        pass: true,
-      };
-    } else {
-      let msg = "";
-      const a = new Int8Array(x);
-      const b = new Int8Array(y);
-      if (a.length !== b.length) {
-        msg = `length expect:${b.length} value:${a.length}`;
-      } else {
-        for (let i = 0; i < a.length; i++) {
-          if (a[i] !== b[i]) {
-            msg += `\nindex:${i} expect:${b[i]} value:${a[i]}`;
-          }
-        }
-      }
-      return {
-        message: () => `expected ${msg}`,
-        pass: false,
-      };
-    }
-  },
-});
 
 function dataViewToBlocks(data) {
   const res = [];
